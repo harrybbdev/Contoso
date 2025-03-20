804a619db25e6bf1d0159497cfa61a5e32b926e0
Harry Bassett-Butt <hbassettbutt@gmail.com>
2025-03-20 14:52:18 +1100

    Initial project structure commit

---
+++ CosmosDB/Domain/Invoice.cs
@@ -0,0 +1,10 @@
+﻿namespace CosmosDB.Domain
+{
+    public class Invoice
+    {
+        public required string Id { get; set; }
+        public required string CustomerName { get; set; }
+        public decimal Amount { get; set; }
+        public required string Status { get; set; } // e.g., Paid, Pending
+    }
+}
